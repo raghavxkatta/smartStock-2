41a78e706165f37a82d160808bd873d086e799da
Raghav Katta <Raghavkatta2005@gmail.com>
2025-07-05 08:31:07 +0530

    Added LenisScroll for smooth effect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import LensisScroll from './components/LensisScroll'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -10,9 +11,11 @@ function App() {
     <>
       <BrowserRouter>
       <Header/>
+      <LensisScroll>
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
+      </LensisScroll>
         <Footer/>
       </BrowserRouter>
     </>
